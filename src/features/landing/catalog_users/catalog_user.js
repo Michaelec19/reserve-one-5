47e88fa43971d3de03ce44b1bbd1803cbef16950
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2026-08-23 20:43:50 -0500

    add generals changes

--- src/features/landing/catalog_users/catalog_user.js
+++ src/features/landing/catalog_users/catalog_user.js
@@ -1,76 +1,7 @@
+/* eslint-disable no-undef */
+import { classesService } from '../../../services/classesService.js'
 import { Alert } from '../../../shared/components/Alert/Alert.js'
 import { capitalize } from '../../../shared/js/utils.js'
-
-const LOCALSTORAGE_KEY = 'lanhua_classes'
-
-const defaultClasses = [
-  {
-    id: crypto.randomUUID(),
-    title: 'rutina',
-    level: 'principiante',
-    capacity: 15,
-    date: '2026-07-02T18:00',
-    dateText: '2 Julio 2026 — Lunes 6 PM',
-    location: 'Medellín',
-    modality: 'grupal',
-    image: '../../../assets/1.png'
-  },
-  {
-    id: crypto.randomUUID(),
-    title: 'combate',
-    level: 'avanzado',
-    capacity: 10,
-    date: '2026-07-03T19:00',
-    dateText: '3 Julio 2026 — Martes 7 PM',
-    location: 'Monterrey',
-    modality: 'grupal',
-    image: '../../../assets/2.png'
-  },
-  {
-    id: crypto.randomUUID(),
-    title: 'taichi',
-    level: 'intermedio',
-    capacity: 20,
-    date: '2026-07-04T17:00',
-    dateText: '4 Julio 2026 — Miércoles 5 PM',
-    location: 'Sabaneta',
-    modality: 'grupal',
-    image: '../../../assets/3.png'
-  },
-  {
-    id: crypto.randomUUID(),
-    title: 'Wuchu general',
-    level: 'intermedio',
-    capacity: 20,
-    date: '2026-07-04T17:00',
-    dateText: '4 Julio 2026 — Miércoles 5 PM',
-    location: 'Sabaneta',
-    modality: 'grupal',
-    image: '../../../assets/3.png'
-  },
-  {
-    id: crypto.randomUUID(),
-    title: 'Taolu (formas)',
-    level: 'intermedio',
-    capacity: 20,
-    date: '2026-07-04T17:00',
-    dateText: '4 Julio 2026 — Miércoles 5 PM',
-    location: 'Laureles',
-    modality: 'grupal',
-    image: '../../../assets/3.png'
-  },
-  {
-    id: crypto.randomUUID(),
-    title: 'Acondicionamiento físico',
-    level: 'intermedio',
-    capacity: 20,
-    date: '2026-07-04T17:00',
-    dateText: '4 Julio 2026 — Miércoles 5 PM',
-    location: 'Laureles',
-    modality: 'grupal',
-    image: '../../../assets/3.png'
-  }    
-]
 
 const ScheduleCardUser = (classItem) => {
   return `
@@ -124,14 +55,7 @@ const ScheduleCardUser = (classItem) => {
 }
 
 const getClasses = () => {
-  const savedData = localStorage.getItem(LOCALSTORAGE_KEY)
-
-  if (!savedData) {
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(defaultClasses))
-    return defaultClasses
-  }
-
-  return JSON.parse(savedData)
+  return classesService.getClasses()
 }
 
 const renderClasses = () => {
